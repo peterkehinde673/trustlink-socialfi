0eafe2bc97e13342d1e9431592beec4434d6db68
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-13 20:40:47 +0100

    Create Leaderboard.tsx

---
+++ src/pages/Leaderboard.tsx
@@ -0,0 +1,15 @@
+function Leaderboard() {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+
+      <ol>
+        <li>Peter - 92</li>
+        <li>Alice - 88</li>
+        <li>John - 75</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Leaderboard;
