42411340d01ff7d6b7ec0cb89cf55edc42556f31
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-13 20:38:58 +0100

    Create Dashboard.tsx

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>TrustLink Dashboard</h1>
+      <p>Total Users: 128</p>
+      <p>Total Endorsements: 584</p>
+      <p>Your Reputation: 92</p>
+    </div>
+  );
+}
+
+export default Dashboard;
