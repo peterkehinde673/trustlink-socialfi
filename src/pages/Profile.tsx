691b9e59580ad6d346ac4f60c5fed8e9f21241dd
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-13 20:40:02 +0100

    Create Profile.tsx

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+function Profile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Name: Peter</p>
+      <p>Reputation: 92</p>
+      <p>Wallet: Connected</p>
+    </div>
+  );
+}
+
+export default Profile;
