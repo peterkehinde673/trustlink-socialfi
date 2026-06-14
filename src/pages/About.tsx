3ff316fa60513afc059d1942f30cca3e91ad69e9
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 07:08:16 +0100

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div>
+      <h1>About TrustLink</h1>
+
+      <p>
+        TrustLink is a decentralized Social-Fi platform where
+        users build reputation through endorsements and community trust.
+      </p>
+    </div>
+  );
+}
+
+export default About;
