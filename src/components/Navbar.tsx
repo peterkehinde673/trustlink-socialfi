d8907a0d6d5f9b183348ebde924b933b516c25ac
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 14:27:12 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav>
-      <h2>TrustLink</h2>
+    <nav style={{ marginBottom: "20px" }}>
+      <Link to="/">Dashboard</Link> |{" "}
+      <Link to="/profile">Profile</Link> |{" "}
+      <Link to="/leaderboard">Leaderboard</Link> |{" "}
+      <Link to="/endorsement">Endorsement</Link> |{" "}
+      <Link to="/about">About</Link>
     </nav>
   );
 }
