364e3983307d92ee7d94a662c944e666e4a1e1cc
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 20:37:37 +0100

    Update UserCard.tsx

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,13 @@
 function UserCard({ name, reputation }: { name: string; reputation: number }) {
   return (
-    <div>
+    <div
+      style={{
+        border: "1px solid #333",
+        borderRadius: "10px",
+        padding: "12px",
+        marginBottom: "10px",
+  }}
+>
       <h3>{name}</h3>
       <p>Reputation: {reputation}</p>
     </div>
