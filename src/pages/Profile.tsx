41ec44086cea287a6c417427cca36dc45622bd2c
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 01:30:07 +0100

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,14 @@
-function Profile() {
+function Leaderboard() {
   return (
     <div>
-      <h1>Your Profile</h1>
-      <p>Name: Peter</p>
-      <p>Wallet: Connected</p>
-      <p>Reputation: 92</p>
+      <h1>Top Trusted Users</h1>
+      <ol>
+        <li>Peter - 92</li>
+        <li>Alice - 88</li>
+        <li>John - 75</li>
+      </ol>
     </div>
   );
 }
 
-export default Profile;
+export default Leaderboard;
