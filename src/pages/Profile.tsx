bc32d597cfb9121813188d4333e82140a0ec7f90
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 13:27:06 +0100

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,14 +4,15 @@ import WalletConnect from "../components/WalletConnect";
 function Profile() {
   return (
     <div>
-      <h1>Your Profile</h1>
+      <h1>Profile</h1>
 
       <p>Name: {user.name}</p>
       <p>Reputation: {user.reputation}</p>
-      <Badge reputation={user.reputation} />
-      <p>Endorsements: {user.endorsements}</p>
-      <p>Wallet: {user.wallet}</p>
 
+      {/* ✅ THIS is how you use Badge */}
+      <Badge reputation={user.reputation} />
+
+      {/* Wallet component */}
       <WalletConnect />
     </div>
   );
