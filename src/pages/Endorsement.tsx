d1fdb73cda4d39d126f4dac99964c862269492f9
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 01:33:13 +0100

    Update Endorsement.tsx

--- src/pages/Endorsement.tsx
+++ src/pages/Endorsement.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function Endorsement() {
+  const [reputation, setReputation] = useState(92);
+
+  const giveEndorsement = () => {
+    setReputation(reputation + 1);
+  };
+
   return (
     <div>
       <h1>Endorse a User</h1>
-      <button>Give Endorsement</button>
+
+      <p>Current Reputation: {reputation}</p>
+
+      <button onClick={giveEndorsement}>
+        Give Endorsement
+      </button>
     </div>
   );
 }
