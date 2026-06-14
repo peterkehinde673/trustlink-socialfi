99c9387ce7336bb7a15ef97007cfe8fadff6ae8e
peterkehinde673 <62162349+peterkehinde673@users.noreply.github.com>
2026-06-14 07:30:59 +0100

    Create ActivityFeed.tsx

---
+++ src/components/ActivityFeed.tsx
@@ -0,0 +1,11 @@
+function ActivityFeed() {
+  return (
+    <ul>
+      <li>Alice endorsed Peter</li>
+      <li>Sarah gained +2 reputation</li>
+      <li>John joined TrustLink</li>
+    </ul>
+  );
+}
+
+export default ActivityFeed;
